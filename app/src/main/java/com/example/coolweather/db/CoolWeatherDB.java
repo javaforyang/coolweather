0ee014095ecf18cff5a29a5fd542fd753449009e
Neil <524816083@qq.com>
2016-03-28 12:52:56 +0800

    database commit.

--- app/src/main/java/com/example/coolweather/db/CoolWeatherDB.java
+++ app/src/main/java/com/example/coolweather/db/CoolWeatherDB.java
@@ -1,7 +1,10 @@
 package com.example.coolweather.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.example.coolweather.model.Province;
 
 /**
  * 操作数据库
@@ -17,6 +20,27 @@ public class CoolWeatherDB {
     private CoolWeatherDB(Context context){
         CoolWeatherOpenHelper dbHelper = new CoolWeatherOpenHelper(context,DB_NAME,null,VERSION);
         db = dbHelper.getWritableDatabase();
+    }
+
+    /**
+     * 获取CoolWeather实例并且防止多线程操作数据库
+     * @param context
+     * @return
+     */
+    public synchronized static CoolWeatherDB getInstance(Context context){
+        if (coolWeatherDB != null){
+            coolWeatherDB = new CoolWeatherDB(context);
+        }
+        return coolWeatherDB;
+    }
+
+    /**
+     * 将Province实例存储到数据库
+     * @param province
+     */
+    public void saveProvince(Province province){
+        
+
     }
 
 }
